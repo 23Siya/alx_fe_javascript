4c7253225ce606e2e314eaab76029cb4e8ccec71
23Siya <156370624+23Siya@users.noreply.github.com>
2025-02-04 13:54:04 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const qoutes = [{text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"},
+                 {text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"},
+                 {text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"}
+                            ];
   
 });
