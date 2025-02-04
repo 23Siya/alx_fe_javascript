024fa1e71222c919376f729391940ec972a5aecb
23Siya <156370624+23Siya@users.noreply.github.com>
2025-02-04 14:07:16 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,6 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const qoutes = [{text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"},
                  {text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"},
                  {text:"jnSDBNX hbhCNDxzN" category :"DJSNCnj"}
-                            ];
+                 ];
+
+const quoteDisplay = document.getElementById("quoteDisplay");
+
+ function showRandomQuote() {
+        if (quotes.length === 0) {
+            quoteDisplay.textContent = "No quotes available.";
+            return;
+        }
+       const randomIndex = Math.floor(Math.random() * quotes.length);
+        quoteDisplay.textContent = `"${quotes[randomIndex].text}" - ${quotes[randomIndex].category}`;
+    }
   
 });
